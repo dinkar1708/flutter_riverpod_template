e8d7b578a6b04e33b67a4c3bc921874d8e8721a0
Maurya <dinkar1708@gmail.com>
2024-02-04 18:56:55 +0900

    dev , prod url setting

---
+++ lib/data/api/api_url_configuration.dart
@@ -0,0 +1,19 @@
+class DevBaseUrl {
+  DevBaseUrl._();
+  static const baseUrlDev = 'https://api.github.com/'; // change dev url here
+  static const appApiKey = 'api key TODO';
+}
+
+class ProdBaseUrl {
+  ProdBaseUrl._();
+  static const baseUrlDev = 'https://api.github.com/'; // change prod url here
+  static const appApiKey = 'api key TODO';
+}
+
+
+class TestBaseUrl {
+  TestBaseUrl._();
+  static const baseUrlDev = 'https://api.github.com/'; // change test url here
+  static const appApiKey = 'api key TODO';
+}
+
