2cfd93cdd72aef6e4cff382f473652610e55a9f4
Maurya <dinkar1708@gmail.com>
2024-03-01 23:41:38 +0900

    define style

---
+++ lib/feature/shared/utils/styles/app_text_style.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_template/feature/shared/utils/styles/app_color.dart';
+
+class AppTextStyle {
+  const AppTextStyle._();
+
+  //body
+  static const TextStyle bodyLarge = TextStyle(
+    fontSize: 20.0,
+  );
+
+  static const TextStyle bodyLargeBold = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    fontSize: 18.0,
+  );
+
+  static const TextStyle bodyMediumBold = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bodySmall = TextStyle(
+    fontSize: 16.0,
+  );
+
+  static const TextStyle bodySmallBold = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+  );
+
+//title
+  static const TextStyle titleLarge = TextStyle(
+    fontSize: 16.0,
+  );
+
+  static const TextStyle titleLargeBold = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle titleMedium = TextStyle(
+    fontSize: 14.0,
+  );
+
+  static const TextStyle titleMediumBold = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle titleSmall = TextStyle(
+    fontSize: 12.0,
+  );
+
+  static const TextStyle titleSmallBold = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.bold,
+  );
+
+//bold
+  static const TextStyle labelLarge = TextStyle(
+    fontSize: 17.0,
+  );
+
+  static const TextStyle labelLargeBold = TextStyle(
+    fontSize: 17.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle labelMedium = TextStyle(
+    fontSize: 15.0,
+  );
+
+  static const TextStyle labelMediumBold = TextStyle(
+    fontSize: 15.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle labelSmall = TextStyle(
+    fontSize: 13.0,
+  );
+
+  static const TextStyle labelSmallBold = TextStyle(
+    fontSize: 13.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final buttonStyle = ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(AppColor.mint2),
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0))));
+}
