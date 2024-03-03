0b22c9b1082aa6dd716ef7a8f9d1f5fe620504c4
Maurya <dinkar1708@gmail.com>
2024-03-03 19:53:10 +0900

    add color based on brightness

--- lib/feature/shared/utils/styles/app_color.dart
+++ lib/feature/shared/utils/styles/app_color.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
 
+// TODO manage readable names for colors
 class AppColor {
-  static const mint2 = Color(0xFF66D4CF);
-  static final Color textPrimaryLight = HexColor("#f4f6f6");
-  static final Color textSecondayLight = HexColor("#d9e2eb");
-  static const gray = Color(0xFFCAC7CC);
-  static const yellow = Color(0xFFFFFF00);
-  static const green = Color(0xFF00FF00);
-  static const red = Color(0xFFFF0000);
+  const AppColor({
+    required this.textPrimary,
+    required this.divider,
+    required this.textSeconday,
+    required this.gray,
+    required this.backgroundPrimary,
+    required this.backgroundSecondary,
+    required this.red,
+  });
+
+  final Color textPrimary;
+  final Color divider;
+  final Color textSeconday;
+  final Color gray;
+  final Color backgroundPrimary;
+  final Color backgroundSecondary;
+  final Color red;
+
+  static const lightColor = AppColor(
+    textPrimary: Color(0xFF66D4CF),
+    divider: Color(0xFFf4f6f6),
+    textSeconday: Color(0xFFd9e2eb),
+    gray: Color(0xFFCAC7CC),
+    backgroundPrimary: Color(0xFFFFFFFF),
+    backgroundSecondary: Color(0xFFFFFFFF),
+    red: Color(0xFFFF0000),
+  );
+
+  static const darkColor = AppColor(
+    textPrimary: Color(0xFF66D4CF),
+    divider: Color(0xFFf4f6f6),
+    textSeconday: Color(0xFFd9e2eb),
+    gray: Color(0xFFCAC7CC),
+    backgroundPrimary: Color(0xFFFFFFFF),
+    backgroundSecondary: Color(0xFFFFFFFF),
+    red: Color(0xFFFF0000),
+  );
+}
+
+extension AppColorExtension on BuildContext {
+  // get color set by brightness
+  AppColor get color => _getColorByBrightness(Theme.of(this).brightness);
+  // decide colors
+  AppColor _getColorByBrightness(Brightness brightness) {
+    switch (brightness) {
+      case Brightness.light:
+        return AppColor.lightColor;
+      case Brightness.dark:
+        return AppColor.darkColor;
+    }
+  }
 }
