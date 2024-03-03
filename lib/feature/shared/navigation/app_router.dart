536c755c294148213a116635252b3edbe1c5f5b0
Maurya <dinkar1708@gmail.com>
2024-03-03 18:13:44 +0900

    use import instead part to fix generate file issue

--- lib/feature/shared/navigation/app_router.dart
+++ lib/feature/shared/navigation/app_router.dart
@@ -1,19 +1,15 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod_template/feature/counter/counter_page.dart';
-import 'package:flutter_riverpod_template/feature/home_page.dart';
-import 'package:flutter_riverpod_template/feature/repository_list/splash/splash_page.dart';
-part 'app_router.gr.dart';
+import 'app_router.gr.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Page,Route')
-class AppRouter extends _$AppRouter {
+class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
         // initial route mange login, logout etc in splash page decided navigation
         AutoRoute(page: SplashRoute.page, initial: true),
         // other pages routes
         AutoRoute(page: HomeRoute.page),
-        // AutoRoute(page: RepositoryListRoute.page),
-        AutoRoute(page: CounterRoute.page)
+        AutoRoute(page: RepositoryListRoute.page),
+        AutoRoute(page: CounterRoute.page),
       ];
 }
