a91a16bfd1d16b0349d67feb4d6e820ae699298f
Maurya <dinkar1708@gmail.com>
2024-03-03 17:05:48 +0900

    defined initial route

--- lib/feature/shared/navigation/app_router.dart
+++ lib/feature/shared/navigation/app_router.dart
@@ -1,14 +1,16 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_template/feature/home_page.dart';
+import 'package:flutter_riverpod_template/feature/repository_list/splash/splash_page.dart';
 part 'app_router.gr.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: HomeRoute.page, initial: true),
-        // AutoRoute(page: CounterRoute.page),
-        // AutoRoute(page: RepositoryListRoute.page)
+        // initial route mange login, logout etc in splash page decided navigation
+        AutoRoute(page: SplashRoute.page, initial: true),
+        // other pages routes
+        AutoRoute(page: HomeRoute.page),
       ];
 }
