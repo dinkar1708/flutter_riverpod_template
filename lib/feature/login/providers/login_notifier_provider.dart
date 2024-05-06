2e4815acac82eb8554d2170c8cc6eb2c20c0e626
Maurya <dinkar1708@gmail.com>
2024-05-06 21:32:42 +0900

    Update login_notifier_provider.dart

--- lib/feature/login/providers/login_notifier_provider.dart
+++ lib/feature/login/providers/login_notifier_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod_template/data/remote/api/api_result_state.dart';
 import 'package:flutter_riverpod_template/feature/login/models/login_request_model.dart';
 import 'package:flutter_riverpod_template/feature/login/models/login_response_model.dart';
@@ -11,13 +12,17 @@ class LoginNotifier extends _$LoginNotifier {
 
   @override
   Future<APIResultState> build() async {
+    debugPrint('login initial state....');
+
     return _state;
   }
 
   Future<LoginResponseModel> login(LoginRequestModel loginRequestModel) async {
+    debugPrint('login requested....');
     _state = APIResultState.loading;
     // Simulate loading delay
     await Future.delayed(const Duration(seconds: 2));
+    debugPrint('login data loaded....');
     _state = APIResultState.result;
     // Return dummy future with dummy response model
     // TODO api call goes here
