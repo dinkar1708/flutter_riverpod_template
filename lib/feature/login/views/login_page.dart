9de908b9ff2f6ba873d7998bab7b7f83f4a1c9c4
Dinakar Maurya <dinkar1708@gmail.com>
2025-10-02 18:01:59 +0900

    Fix warnings

--- lib/feature/login/views/login_page.dart
+++ lib/feature/login/views/login_page.dart
@@ -12,10 +12,7 @@ import 'package:flutter_riverpod_template/feature/shared/utils/ui_utils.dart';
 class LoginPage extends ConsumerStatefulWidget {
   final String title;
 
-  const LoginPage({
-    required this.title,
-    super.key,
-  });
+  const LoginPage({required this.title, super.key});
 
   @override
   ConsumerState<LoginPage> createState() => _LoginPageState();
@@ -36,34 +33,27 @@ class _LoginPageState extends ConsumerState<LoginPage> {
       child: Scaffold(
         body: Container(
           padding: const EdgeInsets.all(20.0),
-          child: _buildLoginFormView(loginState, context),
+          child: _buildLoginFormView(loginState),
         ),
       ),
     );
   }
 
-  Widget _buildLoginFormView(
-      AsyncValue<LoginStateModel> loginState, BuildContext context) {
+  Widget _buildLoginFormView(AsyncValue<LoginStateModel> loginState) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const Text('Login'),
-        const SizedBox(
-          height: 100,
-        ),
+        const SizedBox(height: 100),
         if (loginState.value?.errorMessage != null)
           Text(loginState.value!.errorMessage),
         TextField(
           controller: _userNameController,
-          decoration: const InputDecoration(
-            labelText: 'Username',
-          ),
+          decoration: const InputDecoration(labelText: 'Username'),
         ),
         TextField(
           controller: _passwordController,
-          decoration: const InputDecoration(
-            labelText: 'Password',
-          ),
+          decoration: const InputDecoration(labelText: 'Password'),
           obscureText: true,
         ),
         _buildLoginButtonView(loginState),
@@ -76,28 +66,28 @@ class _LoginPageState extends ConsumerState<LoginPage> {
       onPressed: () async {
         if (loginState.value?.apiResultState == APIResultState.loading) {
           debugPrint(
-              'Previouse click is still in progress...ignoring clicks...');
+            'Previouse click is still in progress...ignoring clicks...',
+          );
           return;
         }
         final loginRequestModel = LoginRequestModel(
           userName: _userNameController.text,
           password: _passwordController.text,
         );
-        loginNotifier.login(loginRequestModel).then((loginStateModel) => {
-              if (loginStateModel.apiResultState == APIResultState.result &&
-                  loginStateModel.loginResponseModel != null)
-                {
-                  showSnackBar(context,
-                      'Login success ${loginStateModel.loginResponseModel!.userName}'),
-                  context.router.replaceAll([HomeRoute(title: 'Home')]),
-                }
-              else
-                {
-                  // show error message as snack bar or dailog anything
-                  showSnackBar(
-                      context, 'Login failed ${loginStateModel.errorMessage}'),
-                }
-            });
+        final loginStateModel = await loginNotifier.login(loginRequestModel);
+        if (!mounted) return;
+        if (loginStateModel.apiResultState == APIResultState.result &&
+            loginStateModel.loginResponseModel != null) {
+          showSnackBar(
+            context,
+            'Login success '
+            '${loginStateModel.loginResponseModel!.userName}',
+          );
+          context.router.replaceAll([HomeRoute(title: 'Home')]);
+        } else {
+          // show error message as snack bar or dailog anything
+          showSnackBar(context, 'Login failed ${loginStateModel.errorMessage}');
+        }
       },
       child: loginState.value?.apiResultState == APIResultState.loading
           ? const CircularProgressIndicator()
